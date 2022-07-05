afe89b11899fe215a57c22bf84998a7a59d7e687
lephu13502 <lephu13502@gmail.com>
2022-07-05 23:58:55 +0700

    update

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { setDoc,doc,serverTimestamp} from 'firebase/firestore';
 import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
@@ -27,6 +28,10 @@ function SignUp() {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password)
             const user = userCredential.user
             updateProfile(auth.currentUser, {displayName: name})
+            const formDataCopy = {...formData}
+            delete formDataCopy.password
+            formDataCopy.timestamp = serverTimestamp()
+            await setDoc(doc(db, 'users', user.uid), formDataCopy)
             navigate('/')
         } catch (error) {
             console.log(error)
