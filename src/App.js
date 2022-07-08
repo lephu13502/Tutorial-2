30534e352fa56481e36918a38957b1ce839b4090
lephu13502 <lephu13502@gmail.com>
2022-07-08 16:29:02 +0700

    new

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import Profile from './pages/Profile'
 import Offers from './pages/Offers'
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
-
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -28,6 +29,8 @@ function App() {
           <Route path='/offers' element={<Offers/>}/>
           <Route path='/category/:categoryName' element={<Category/>}/>
           <Route path='/create-listing' element={<CreateListing/>}/>
+          <Route path='/category/:categoryName/:listingId' element={<Listing />}/>
+          <Route path='/contact/:landlordId' element={<Contact/>}/>
         </Routes>
         <Navbar/>
       </Router>
