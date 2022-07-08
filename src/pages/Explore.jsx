5a93319079914393175d729de5056b6301df55bd
lephu13502 <lephu13502@gmail.com>
2022-07-08 17:27:55 +0700

    new

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,4 @@
 import {Link} from 'react-router-dom';
-import Slider from '../components/Slider';
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
 function Explore() {
